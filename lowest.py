e5dcaec46b4c564d6f73e9f4bfc326264c625432
Adri-marq <marquez.lozano.adrian@iescamas.es>
2025-09-16 20:21:00 +0200

    Add files via upload

---
+++ lowest.py
@@ -0,0 +1,10 @@
+num1 = int(input("enter the firts number:"))
+num2 = int(input("enter the second number:"))
+
+if num1 <= num2:
+    lowest = num1
+else:
+    lowest = num2
+    
+    print(" the lowest number is " + str(lowest))
+    
